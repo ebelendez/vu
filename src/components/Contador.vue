fff3e2843a13218b1b1354f464f9c8aaad4c5a2c
ebelen <enrique@codigo-base.com.mx>
2026-02-12 18:17:57 -0600

    no message

---
+++ src/components/Contador.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'
+const contador = ref(0);
+</script>
+
+<template>
+  <h1>Hola Mundo</h1>
+  <br>
+  <button @click="contador--" :disabled="contador == 0"> - </button>
+  {{ contador }}
+  <button @click="contador++"> + </button>
+  <br>
+</template>
+
+<style scoped>
+</style>
